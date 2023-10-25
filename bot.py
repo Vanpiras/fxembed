f8afe7d9432dae8f34e1d0fc597cfcca5cf9698f
Alexandre Delorme <alexandre.delorme@epita.fr>
2023-10-25 17:09:29 +0200

    ADD: twitter and tiktok vx added

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import requests
 
 f = open('.token')
 token = f.read().strip()
@@ -11,9 +12,17 @@ class DiscordClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if message.content == 'ping':
-            await message.channel.send('pong')
+        if 'https://twitter.com' in message.content:
+            await message.channel.send(message.content.replace('https://twitter.com', 'https://vxtwitter.com'))
             await message.delete()
+        if 'https://vm.tiktok.com' in message.content:
+            r = requests.get(message.content)
+            await message.channel.send(r.url.replace('https://www.tiktok.com', 'https://www.vxtiktok.com'))
+            await message.delete()
+        if 'https://www.tiktok.com' in message.content:
+            await message.channel.send(message.content.replace('https://www.tiktok.com', 'https://www.vxtiktok.com'))
+            await message.delete()
+
 
 intents = discord.Intents.default()
 intents.message_content = True
