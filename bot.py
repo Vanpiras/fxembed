945b1f5e3d717eded6758b81c22065ce522ba41e
Alexandre Delorme <alexandre.delorme@epita.fr>
2023-10-25 19:49:15 +0200

    ADD: prefix for sent message and ddinstagram

--- bot.py
+++ bot.py
@@ -12,15 +12,19 @@ class DiscordClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
+        prefix = f"<@{message.author.id}> sent:\n" 
         if 'https://twitter.com' in message.content:
-            await message.channel.send(message.content.replace('https://twitter.com', 'https://vxtwitter.com'))
+            await message.channel.send(prefix + message.content.replace('https://twitter.com', 'https://vxtwitter.com'), silent=True)
             await message.delete()
         if 'https://vm.tiktok.com' in message.content:
             r = requests.get(message.content)
-            await message.channel.send(r.url.replace('https://www.tiktok.com', 'https://www.vxtiktok.com'))
+            await message.channel.send(prefix + r.url.replace('https://www.tiktok.com', 'https://www.vxtiktok.com'), silent=True)
             await message.delete()
         if 'https://www.tiktok.com' in message.content:
-            await message.channel.send(message.content.replace('https://www.tiktok.com', 'https://www.vxtiktok.com'))
+            await message.channel.send(prefix + message.content.replace('https://www.tiktok.com', 'https://www.vxtiktok.com'), silent=True)
+            await message.delete()
+        if 'https://instagram.com' in message.content:
+            await message.channel.send(prefix + message.content.replace('https://instagram.com', 'https://ddinstagram.com'), silent=True)
             await message.delete()
 
 
